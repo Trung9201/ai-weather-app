bcc8bef49eda181a4c3a2bae67de1d248cc7ee82
TrungHips <nguyenquoctrung0902@gmail.com>
2023-04-05 15:28:36 +0700

    done

--- index.js
+++ index.js
@@ -1,92 +1,60 @@
-const wrapper = document.querySelector(".wrapper"),
-  inputPart = wrapper.querySelector(".input-part"),
-  infoTxt = inputPart.querySelector(".info-txt"),
-  inputField = inputPart.querySelector("input"),
-  locationBtn = inputPart.querySelector("button"),
-  weatherPart = wrapper.querySelector(".weather-part"),
-  wIcon = wrapper.querySelector(".weather-part img"),
-  btnBack = wrapper.querySelector("header i");
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
 
-const apiKey = "eae716dc50073d95e2f23037e30a6283";
-let api;
+const inputCity = $('#input-city')
+const btnSearch = $('#btn-search')
+const textError = $('.text-error')
+const cityName = $('.city-name')
+const imgIcon = $('#img-icon')
+const temperature = $('.temperature')
+const doam = $('.doam')
+const numbTemperature = $('.numb-temperature')
 
-inputField.addEventListener("keyup", (e) => {
-  if (e.key == "Enter" && inputField.value != "") {
-    requestApi(inputField.value);
-  }
-});
 
-locationBtn.onclick = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+let country = ''
+const apiKey = '06e0ba0ffd7a042c3a1f2881d3fee86e'
+
+async function getApi(city) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+  const data = await response.json()
+  console.log(data);
+  weatherDetails(data)
+}
+
+function weatherDetails(data) {
+  if (data.cod === '404') {
+    textError.style.display = 'block'
+    textError.textContent = `${country} isn't a valid city name!`
+    inputCity.value = ''
   } else {
-    alert("Your brower not support geolacation api");
-  }
-};
+    textError.style.display = 'none'
+    cityName.textContent = `${data.name}, ${data.sys.country}`
 
-function onSuccess(position) {
-  const { latitude, longitude } = position.coords;
-  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
-  fetchData();
-}
-
-function onError(error) {
-  infoTxt.innerText = error.message;
-  infoTxt.classList.add("error");
-}
-
-function requestApi(city) {
-  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  fetchData();
-}
-
-function fetchData() {
-  infoTxt.innerText = "Getting weather details...";
-  infoTxt.classList.add("pedding");
-  fetch(api)
-    .then((response) => response.json())
-    .then((result) => weatherDetails(result));
-}
-
-function weatherDetails(info) {
-  infoTxt.classList.replace("pedding", "error");
-  if (info.cod == "404") {
-    infoTxt.innerText = `${inputField.value} isn't a valid city name`;
-  } else {
-    const city = info.name;
-    const country = info.sys.country;
-    const { description, id } = info.weather[0];
-    const { feels_like, humidity, temp } = info.main;
+    const { description, id } = data.weather[0];
+    const { feels_like, humidity, temp } = data.main;
 
     if (id == 800) {
-      wIcon.src = "/weather-app/icons/clear.svg";
+      imgIcon.src = "./icons/clear.svg";
     } else if (id >= 200 && id <= 232) {
-      wIcon.src = "/weather-app/icons/strom.svg";
+      imgIcon.src = "./icons/strom.svg";
     } else if (id >= 600 && id <= 622) {
-      wIcon.src = "/weather-app/icons/snow.svg";
+      imgIcon.src = "./icons/snow.svg";
     } else if (id >= 701 && id <= 781) {
-      wIcon.src = "/weather-app/icons/haze.svg";
+      imgIcon.src = "./icons/haze.svg";
     } else if (id >= 801 && id <= 804) {
-      wIcon.src = "/weather-app/icons/cloud.svg";
+      imgIcon.src = "./icons/cloud.svg";
     } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)) {
-      wIcon.src = "/weather-app/icons/rain.svg";
+      imgIcon.src = "./icons/rain.svg";
     }
 
-    wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
-    wrapper.querySelector(".weather").innerText = description;
-    wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
-    weatherPart.querySelector(".feels .temp .numb").innerText =
-      Math.floor(feels_like);
-    weatherPart.querySelector(
-      ".humidity .temp .numb"
-    ).innerText = `${humidity}%`;
-
-    infoTxt.classList.remove("pedding", "error");
-    wrapper.classList.add("active");
+    temperature.textContent = Math.floor(temp)
+    numbTemperature.textContent = Math.floor(feels_like)
+    doam.textContent = Math.floor(humidity)
+    inputCity.value = ''
   }
 }
 
-btnBack.addEventListener("click", () => {
-  wrapper.classList.remove("active");
-  inputField.value = "";
-});
+btnSearch.onclick = () => {
+  country = inputCity.value
+  getApi(country)
+}
